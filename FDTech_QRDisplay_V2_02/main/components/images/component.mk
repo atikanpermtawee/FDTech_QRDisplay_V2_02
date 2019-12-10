534703622efd0845326465beec72258f394d1401
macbook-atk <macbook-atk@atikans-mbp>
2019-12-10 16:06:57 +0700

    first commit

--- FDTech_QRDisplay_V2_02/main/components/images/component.mk
+++
@@ -1,9 +0,0 @@
-#
-# Main Makefile. This is basically the same as a component makefile.
-#
-# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-
-COMPONENT_SRCDIRS := . 
-COMPONENT_ADD_INCLUDEDIRS := .
-
-COMPONENT_EMBED_FILES := bluepay.jpg bluepay_press.jpg bottom1.jpg bottom1_press.jpg bottom2.jpg bottom2_press.jpg promppay.jpg promppay_press.jpg rabbitline.jpg rabbitline_press.jpg topmenu.jpg truemoney.jpg truemoney_press.jpg payment_pg.jpg
