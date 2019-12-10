2bf4c836bfd343bd659759c93da4878feeddee2c
macbook-atk <macbook-atk@atikans-mbp>
2019-12-10 16:03:56 +0700

    First Commit

---
+++ main/spiffs.h
@@ -0,0 +1,18 @@
+#ifndef SPIFFS_H_INCLUDED
+#define SPIFFS_H_INCLUDED
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ============================================================================== */
+/* ============================================================================== */
+void app_spiffs(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SPIFFS_H_INCLUDED */
